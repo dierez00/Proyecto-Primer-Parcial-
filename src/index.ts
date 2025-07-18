d5ef51114adf52c5be8560ce0f57c1e2eae36932
Diego Antunez <dierez00@gmail.com>
2025-07-18 05:22:54 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import morgan from 'morgan';
 import routes from './routes/routes';
 import connectDB from './config/bd';
 import cors from 'cors';
+
 const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
+// Configura CORS solo para el frontend en Railway
+app.use('/app', cors({
+  origin: 'https://front-production-09a4.up.railway.app',
+  credentials: true // por si usas cookies o headers de autorización
+}), routes);
+
 app.use(express.json());
-app.use(cors());
 app.use(morgan('dev'));
-app.use('/app', routes);
 
 connectDB().then(() => {
   app.listen(PORT, '0.0.0.0', () => {
